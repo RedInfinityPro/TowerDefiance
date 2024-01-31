d51e0ff4d37c4de97f879cd5641972c8c7adfca4
Statute8234 <76547158+Statute8234@users.noreply.github.com>
2024-01-30 21:42:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ import random, time, sys
 from perlin_noise import PerlinNoise
 from pathfinding.core.grid import Grid
 from pathfinding.finder.a_star import AStarFinder
+import time
 # screen
 pygame.init()
 mixer.init()
 screenWidth, screenHeight = 700, 700
-screen = pygame.display.set_mode((screenWidth, screenHeight))
+screen = pygame.display.set_mode((screenWidth, screenHeight), pygame.RESIZABLE)
 pygame.display.set_caption("Game")
 clock = pygame.time.Clock()
 pygame.display.flip()
@@ -46,10 +47,10 @@ class MainGrid:
         end_y = int((screen.get_height() - self.grid_offset[1]) / self.cell_size) + 1
         # draw line
         for x in range(start_x, end_x):
-            pygame.draw.line(screen, BLACK, (x * self.cell_size + self.grid_offset[0], 0),
+            pygame.draw.line(screen, RED, (x * self.cell_size + self.grid_offset[0], 0),
                              (x * self.cell_size + self.grid_offset[0], screen.get_height()))
         for y in range(start_y, end_y):
-            pygame.draw.line(screen, BLACK, (0, y * self.cell_size + self.grid_offset[1]),
+            pygame.draw.line(screen, RED, (0, y * self.cell_size + self.grid_offset[1]),
                              (screen.get_width(), y * self.cell_size + self.grid_offset[1]))
 grid = MainGrid(cell_size=50)
 # ground Segment
@@ -91,7 +92,7 @@ class Ground:
         self.height = screen_height // cell_size
         self.segmentList = []
         self.ground_data = self.generate_ground()
-        self.Build()
+        self.build()
 
     def generate_ground(self):
         returnList = []
@@ -100,7 +101,7 @@ class Ground:
         octaves = random.randint(1, 6)
         seed = random.randint(1, 100)
         noise = PerlinNoise(octaves=octaves, seed=seed)
-        water_threshold = random.uniform(0.0, 0.1) # user 0.2 - 0.5 if you want realisum
+        water_threshold = random.triangular(0.0, 0.2, 0.5) # user 0.2 - 0.5 if you want realisum
 
         for i in range(self.width):
             for ii in range(self.height):
@@ -108,6 +109,7 @@ class Ground:
                 cell_y = ii / self.height
                 cell_height = noise([cell_x / freq, cell_y / freq]) * amp
                 brightness = (cell_height + amp) / (2 * amp)
+                brightness = max(0, min(1, brightness))
                 # Adjust color based on height and add some randomness
                 color_value = int(brightness * 255) + random.randint(-10, 10)
                 color_value = max(0, min(255, color_value))
@@ -122,20 +124,23 @@ class Ground:
     def regenerate_map(self):
         self.segmentList = []
         self.ground_data = self.generate_ground()
-        self.Build()
+        self.build()
 
-    def Build(self):
+    def build(self):
         for item in self.ground_data:
             cell_x, cell_y = item[0]
             cell_height = item[1]
             color = item[2]
-            self.segment = Segment(cell_x * self.screen_width,cell_y * self.screen_height,50,50,LIME,color)
+            segment_width = self.screen_width // self.width
+            segment_height = self.screen_height // self.height
+
+            self.segment = Segment(cell_x * self.screen_width, cell_y * self.screen_height, segment_width, segment_height, LIME, color)
             self.segmentList.append(self.segment)
 
     def draw(self,screen):
         for item in self.segmentList:
             item.draw(screen)
-    
+
     def handle_event(self, event):
         global showFull_map
         for item in self.segmentList:
@@ -194,10 +199,6 @@ class SettingButton:
 
     def reset(self):
         self.clicked = False
-home = SettingButton(0, screenHeight - 50, 50, 50, r"New folder\images\hut.png")
-settings = SettingButton(50, screenHeight -50, 50, 50, r"New folder\images\gear.png")
-buildSettings = SettingButton(0, screenHeight - 100, 50, 50, r"New folder\images\maintenance.png")
-show = SettingButton(50, screenHeight - 100, 50, 50, r'New folder\images\view.png')
 # button
 class Button:
     def __init__(self, x, y, width, height, text, active_color, inactive_color):
@@ -232,6 +233,184 @@ class Button:
     def reset(self):
         self.clicked = False
         self.color = self.inactive_color
+# SquareGrid class
+class SquareGrid:
+    def __init__(self, x, y, width, height, square_size, padding):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.square_size = square_size
+        self.padding = padding
+        self.squares = []
+    def create_squares(self):
+        num_squares_x = (self.width - self.padding) // (self.square_size + self.padding)
+        num_squares_y = (self.height - self.padding) // (self.square_size + self.padding)
+        for i in range(num_squares_x):
+            for j in range(num_squares_y):
+                square_x = self.x + i * (self.square_size + self.padding) + self.padding
+                square_y = self.y + j * (self.square_size + self.padding) + self.padding
+                self.squares.append((square_x, square_y, self.square_size))
+    def draw(self, screen):
+        for square in self.squares:
+            pygame.draw.rect(screen, DARK_GRAY, pygame.Rect(square[0], square[1], square[2], square[2]))
+    def update_position(self, new_x, new_y):
+        self.x = new_x
+        self.y = new_y
+        self.create_squares()
+# Helper class for draggable objects
+class DraggableImageButton:
+    def __init__(self, x, y, width, height, image_path, cell_size, window_width, window_height, square_grid, panel_x, panel_width):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.image_original = pygame.image.load(image_path)
+        self.image_original = pygame.transform.scale(self.image_original, (width, height))
+        self.image = self.image_original.copy()
+        self.clicked = False
+        self.offset = (0, 0)
+        self.cell_size = cell_size
+        self.window_width = window_width
+        self.window_height = window_height
+        self.square_grid = square_grid
+        self.panel_x = panel_x
+        self.panel_width = panel_width
+        self.snap_to_square_grid = True
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect.topleft)
+
+    def update_size(self, square):
+        if self.snap_to_square_grid and len(square) == 4 and square[2] == square[3]:
+            self.image = pygame.transform.scale(self.image_original, (square[2], square[2]))
+
+    def snap_to_grid(self):
+        if self.rect.colliderect(pygame.Rect(self.panel_x, 0, self.panel_width, self.window_height)):
+            self.snap_to_square_grid = True
+            button_center = self.rect.center
+            for square in self.square_grid.squares:
+                square_rect = pygame.Rect(square[0], square[1], square[2], square[2])
+                if square_rect.collidepoint(button_center):
+                    self.rect.x = square[0]
+                    self.rect.y = square[1]
+                    self.update_size(square)
+        else:
+            self.rect.x = round(self.rect.x / self.cell_size) * self.cell_size
+            self.rect.y = round(self.rect.y / self.cell_size) * self.cell_size
+            self.image = self.image_original.copy()
+            self.snap_to_square_grid = False
+    
+    def limit_position(self):
+        self.rect.x = max(0, min(self.rect.x, self.window_width - self.rect.width))
+        self.rect.y = max(0, min(self.rect.y, self.window_height - self.rect.height))
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                self.clicked = True
+                self.offset = (event.pos[0] - self.rect.x, event.pos[1] - self.rect.y)
+        elif event.type == pygame.MOUSEBUTTONUP:
+            self.clicked = False
+            self.snap_to_grid()
+            self.limit_position()
+        elif event.type == pygame.MOUSEMOTION:
+            if self.clicked:
+                self.rect.x = event.pos[0] - self.offset[0]
+                self.rect.y = event.pos[1] - self.offset[1]
+                self.limit_position()
+                self.snap_to_grid()
+# panel
+class Panel:
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.hidePannel_origPos = (50,0)
+        self.hidePannel = Button(self.hidePannel_origPos[0],self.hidePannel_origPos[1],50,50,"<-",DARK_GRAY,RED)
+        self.home = SettingButton(0, screenHeight - 50, 50, 50, r"New folder\images\hut.png")
+        self.settings = SettingButton(50, screenHeight -50, 50, 50, r"New folder\images\gear.png")
+        self.buildSettings = SettingButton(0, screenHeight - 100, 50, 50, r"New folder\images\maintenance.png")
+        self.show = SettingButton(50, screenHeight - 100, 50, 50, r'New folder\images\view.png')
+        self.squareGrid = SquareGrid(0, 60, self.width, self.height - 160, 35, 10)
+        self.squareGrid.create_squares()
+        self.testButton = DraggableImageButton(screenWidth / 2, screenHeight / 2,50,50,r"New folder\images\gear.png",grid.cell_size,screenWidth, screenHeight, self.squareGrid, self.width,5)
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
+        if not(self.testButton.snap_to_square_grid):
+            self.testButton.draw(screen)
+
+        self.hidePannel.draw(screen)
+        if self.hidePannel.x == self.hidePannel_origPos[0] and self.hidePannel.y == self.hidePannel_origPos[1]:
+            self.home.draw(screen)
+            self.settings.draw(screen)
+            self.buildSettings.draw(screen)
+            self.show.draw(screen)
+            self.squareGrid.draw(screen)
+            if self.testButton.snap_to_square_grid:
+                self.testButton.draw(screen)
+    
+    def buttonHandle_event(self, event):
+        global showSettings, can_build, show_grid
+        if showSettings == False:
+            self.home.handle_event(event)
+            if self.home.clicked:
+                self.home.update_image(r"New folder\images\home.png")
+                self.home.reset()
+            elif not(self.home.clicked):
+                self.home.update_image(r"New folder\images\hut.png")
+            self.settings.handle_event(event)
+            if self.settings.clicked:
+                showSettings = True
+                self.settings.update_image(r"New folder\images\gear(1).png")
+                self.settings.reset()
+            elif not(self.settings.clicked):
+                self.settings.update_image(r"New folder\images\gear.png")
+            self.buildSettings.handle_event(event)
+            if self.buildSettings.clicked:
+                can_build = not(can_build)
+                if can_build:
+                    self.buildSettings.update_image(r"New folder\images\tools.png")
+                else:
+                    self.buildSettings.update_image(r"New folder\images\maintenance.png")
+                self.buildSettings.reset()
+            self.show.handle_event(event)
+            if self.show.clicked:
+                show_grid = not(show_grid)
+                if show_grid:
+                    self.show.update_image(r"New folder\images\hidden.png")
+                else:
+                    self.show.update_image(r"New folder\images\view.png")
+                self.show.reset()
+
+    def move_buttons(self):
+        self.show.clicked = not(self.show.clicked)
+        self.buildSettings.clicked = not(self.buildSettings.clicked)
+        self.width, self.height = 100, screenHeight
+        self.home.y, self.settings.y = screenHeight - 50, screenHeight - 50
+        self.buildSettings.y, self.show.y = screenHeight - 100, screenHeight - 100
+
+    def update(self):
+        self.move_buttons()
+        self.testButton.window_height, self.testButton.window_width = screenHeight, screenWidth
+
+    def handle_event(self, event):
+        self.hidePannel.handle_event(event)
+        if self.hidePannel.x == self.hidePannel_origPos[0] and self.hidePannel.y == self.hidePannel_origPos[1]:
+            self.buttonHandle_event(event)
+        
+        if self.hidePannel.clicked:
+            if self.hidePannel.x == self.hidePannel_origPos[0] and self.hidePannel.y == self.hidePannel_origPos[1]:
+                self.hidePannel.x, self.hidePannel.y = 0, 0
+                self.hidePannel.text = "->"
+                self.x, self.y = -self.width, 0
+            else:
+                self.hidePannel.x, self.hidePannel.y = self.hidePannel_origPos
+                self.hidePannel.text = "<-"
+                self.x, self.y = 0, 0
+            self.hidePannel.reset()
+        self.testButton.handle_event(event)
+panel = Panel(0,0, 100, screenWidth, WHITE)
 #menu
 class MenuWindow:
     def __init__(self, width, height, position=(0, 0)):
@@ -303,6 +482,10 @@ class MenuWindow:
                 self.sound_effects_output.enable()
                 self.frame_rate_output.enable()
 
+    def update(self):
+        self.width, self.height = screenWidth, screenHeight
+        self.close_button.x, self.close_button.y = self.x + self.width - 40, self.y
+
     def handle_event(self, event):
         global showSettings
         if self.visible:
@@ -353,37 +536,9 @@ while running:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_r:
                 terrain_generator.regenerate_map()
-        # settings events
-        if showSettings == False:
-            home.handle_event(event)
-            if home.clicked:
-                home.update_image(r"New folder\images\home.png")
-                home.reset()
-            elif not(home.clicked):
-                home.update_image(r"New folder\images\hut.png")
-            settings.handle_event(event)
-            if settings.clicked:
-                showSettings = True
-                settings.update_image(r"New folder\images\gear(1).png")
-                settings.reset()
-            elif not(home.clicked):
-                settings.update_image(r"New folder\images\gear.png")
-            buildSettings.handle_event(event)
-            if buildSettings.clicked:
-                can_build = not(can_build)
-                if can_build:
-                    buildSettings.update_image(r"New folder\images\tools.png")
-                else:
-                    buildSettings.update_image(r"New folder\images\maintenance.png")
-                buildSettings.reset()
-            show.handle_event(event)
-            if show.clicked:
-                show_grid = not(show_grid)
-                if show_grid:
-                    show.update_image(r"New folder\images\hidden.png")
-                else:
-                    show.update_image(r"New folder\images\view.png")
-                show.reset()
+        elif event.type == pygame.VIDEORESIZE:
+                screenWidth, screenHeight = event.size
+        panel.handle_event(event)
         # windows events
         menuWindow.handle_event(event)
         # map events
@@ -397,10 +552,7 @@ while running:
         grid.draw(screen)
     # menu buttons
     if showSettings == False:
-        home.draw(screen)
-        settings.draw(screen)
-        buildSettings.draw(screen)
-        show.draw(screen)
+        panel.draw(screen)
     else:
         # windows
         menuWindow.visible = True
